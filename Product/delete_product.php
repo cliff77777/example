90033fd316827bceb27346059fe9693ff5e73e2e
cliff77777 <s19910507@gmail.com>
2022-02-28 04:56:58 +0800

    修正刪除商品sql語法

--- Product/delete_product.php
+++ Product/delete_product.php
@@ -3,12 +3,20 @@ require_once "../includes/config.php";
 $id=$_GET["id"];
 
 $sql="UPDATE product SET valid=0 WHERE id=$id";
-// $result=$conn->query($sql);
+$stmt=$db_host->prepare($sql);
 
-if($conn->query($sql)===TRUE){
-    header("location:productList.php");
-}else {
-    echo "刪除資料失敗: " . $conn->error;
+
+
+try{
+    $stmt->execute();
+    echo "刪除商品成功";
+
+}catch(PDOExecption $e){
+    echo "error .<br>";
+    echo "刪除商品錯誤:".$e->getMessage()."<br>";
 }
+$db_host=NULL;
+
+header("location:productList.php");
 
 ?>
